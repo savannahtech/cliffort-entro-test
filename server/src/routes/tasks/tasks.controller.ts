0c03501a264091d688b4aa5b64ff74a55cce991b
Clifford-coder <cliffowusu2727@gmail.com>
2023-08-16 09:18:34 +0000

    feat: Implemented filtering based on search

--- server/src/routes/tasks/tasks.controller.ts
+++ server/src/routes/tasks/tasks.controller.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from '@prisma/client';
-import { route } from '../../_services/route';
 import { TasksService } from './tasks.service';
+import { route } from '../../_services/route';
+import { RequestData } from 'src/_services/types';
+
+export interface TaskListQuery {
+	search?: string;
+}
 
 const service = new TasksService();
-
 export class TasksController {
 	static _prismaClient = new PrismaClient();
 
 	@route()
-	async getTasks() {
-		return service.getTasks();
+	async getTasks({ query }: RequestData<unknown, TaskListQuery>) {
+		return service.getTasks(query);
 	}
 }
--- server/src/routes/tasks/tasks.service.ts
+++ server/src/routes/tasks/tasks.service.ts
@@ -1,11 +1,38 @@
 import { PrismaClient } from '@prisma/client';
+import { TaskListQuery } from './tasks.controller';
 
 export class TasksService {
 	get _prismaClient() {
 		return new PrismaClient();
 	}
 
-	async getTasks() {
-		return this._prismaClient.task.findMany({ include: { relatedTasks: true } });
+	async getTasks(query: TaskListQuery) {
+		const filter: any = {};
+
+		if (query.search) {
+			filter.OR = [
+				{
+					title: {
+						contains: query.search,
+						mode: 'insensitive',
+					},
+				},
+				{
+					assigneeName: {
+						contains: query.search,
+						mode: 'insensitive',
+					},
+				},
+			];
+		}
+
+		const tasks = await this._prismaClient.task.findMany({
+			orderBy: {
+				creationDate: 'desc',
+			},
+			where: filter,
+		});
+
+		return { tasks, count: tasks.length };
 	}
 }
