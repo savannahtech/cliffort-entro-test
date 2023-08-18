fd7b8a4e5f2f6ad6b61da8d7b17d82397014da33
Clifford-coder <cliffowusu2727@gmail.com>
2023-08-18 00:26:59 +0000

    feat: added @faker-js

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,15 @@
 import { createServer } from './app';
 
-(() => {
+export function startServer() {
 	const server = createServer();
 
-	const port = process.env.PORT ?? '4444';
+	const port = process.env.PORT || '4444';
 
 	// TODO: initialize DB and other stuff here
+
 	server.listen(port, () => {
 		console.log(`🎉 server running on port http://localhost:${port}`);
 	});
-})();
+}
+
+startServer();
